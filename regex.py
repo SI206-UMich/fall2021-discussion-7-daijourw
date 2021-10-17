4fbd7fd26c2f2c4aa4f09cb632b063997d666d21
daijourw <daijourw@umich.edu>
2021-10-16 21:28:56 -0400

    first2 work

--- regex.py
+++ regex.py
@@ -21,36 +21,40 @@ def read_file(filename):
 
 def find_word(string_list):
     """ Return a list of words that contain three digit numbers in the middle. """
-
+    lst = []
     # initialize an empty list
 
     # define the regular expression
-
+    regex = '\S+\d{3}\S+'
     # loop through each line of the string list 
-
+    for line in string_list:
     # find all the words that match the regular expression in each line
-    
+        matchlst = re.findall(regex, line)
     # loop through the found words and add the words to your empty list 
-
+        for word in matchlst:
+            lst += word
     #return the list of all words that start with the letter B, E, or T
-    pass
+    return re.findall('^B|E|T', lst)
+    #pass
 
 
 def find_days(string_list):
     """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """  
-
     # initialize an empty list
-
+    somelst = []
     # define the regular expression
-
+    regex = '^\d{2}/\d{2}/\d{4}'
     # loop through each line of the string list
-    
-    # find all the dates that match the regular expression in each line
-    
+    for line in string_list:
+        # find all the dates that match the regular expression in each line
+        matchlst = re.findall(regex, line)
     # loop through the found dates and only add the days to your empty list 
-    
+        for item in matchlst:
+            somelst += item[:2]
+
     #return the list of days
     pass
+    return somelst
 
 def find_domains(string_list):
     """ Return a list of web address domains from the list of strings the domains of a wbsite are after www. """
@@ -58,7 +62,7 @@ def find_domains(string_list):
     # initialize an empty list
 
     # define the regular expression
-
+    #after www.
     # loop through each line of the string list
 
     # find all the domains that match the regular expression in each line
